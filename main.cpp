df833a00c0654fa1fac34f29167ea7cf6742cae7
Iwan Logutenko <looogutenko@gmail.com>
2021-12-29 20:05:37 +0300

    Метод main

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //cout << "" << endl;
+    //вызов функции
+    int alg;
+
+    cout << "Введите номер алгоритма:" << endl;
+    cin >> alg;
+
+    switch (alg)
+    {
+        case 1: /*вызов модуля*/ break;
+        case 2: /*вызов модуля*/ break;
+        case 3: /*вызов модуля*/ break;
+        case 4: /*вызов модуля*/ break;
+        default: cout << "Вы ошиблись с номером." << endl;
+    }
+
+    return 0;
+}
